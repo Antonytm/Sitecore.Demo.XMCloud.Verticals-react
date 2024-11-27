f7b43b476e7432c7b80826ff5607814205c11d8b
Antonytm <antontishchenko@gmail.com>
2024-11-27 18:49:41 +0200

    Revert Astro config, leave dependencies

--- src/sxastarter_astro_empty/astro.config.mjs
+++ src/sxastarter_astro_empty/astro.config.mjs
@@ -1,32 +1,8 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import node from "@astrojs/node";
-
-const reactConfig = {
-  babel: {
-    presets: [
-      [
-        "@babel/preset-react",
-        {
-          runtime: "classic",
-        },
-      ],
-    ],
-    plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
-  },
-};
+// @ts-check
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(reactConfig)],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
-  outDir: "./dist",
-  security: {
-    checkOrigin: false,
-  },
   server: {
     port: 3000,
   },
