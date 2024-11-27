72d8b00e198269f026d9bc619ea8a0585e2b9bfa
Antonytm <antontishchenko@gmail.com>
2024-11-27 17:50:04 +0200

    Adding more dependencies

--- src/sxastarter_astro_empty/astro.config.mjs
+++ src/sxastarter_astro_empty/astro.config.mjs
@@ -1,8 +1,32 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import node from "@astrojs/node";
+
+const reactConfig = {
+  babel: {
+    presets: [
+      [
+        "@babel/preset-react",
+        {
+          runtime: "classic",
+        },
+      ],
+    ],
+    plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
+  },
+};
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react(reactConfig)],
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  outDir: "./dist",
+  security: {
+    checkOrigin: false,
+  },
   server: {
     port: 3000,
   },
