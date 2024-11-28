16988488c737640119bba75db1ee29ffbe54e2bd
Antonytm <antontishchenko@gmail.com>
2024-11-28 13:21:51 +0200

    Revert changes

--- src/sxastarter/astro.config.mjs
+++ src/sxastarter/astro.config.mjs
@@ -2,29 +2,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
-// https://astro.build/config
-const reactConfig = {
-  babel: {
-    presets: [
-      [
-        "@babel/preset-react",
-        {
-          runtime: "classic",
-        },
-      ],
-    ],
-    plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
-  },
-};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(reactConfig)],
+  integrations: [react()],
   output: "server",
   adapter: node({
     mode: "standalone",
   }),
-  outDir: "./dist",
   security: {
     checkOrigin: false,
   },
