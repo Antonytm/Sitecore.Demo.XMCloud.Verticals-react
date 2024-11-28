80be46d3be27972d8b275bbeba0221f74c8fbc77
Antonytm <antontishchenko@gmail.com>
2024-11-28 14:05:21 +0200

    Attempt to fix build

--- src/sxastarter/astro.config.mjs
+++ src/sxastarter/astro.config.mjs
@@ -1,31 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import node from "@astrojs/node";
 
-const reactConfig = {
-  babel: {
-    presets: [
-      [
-        "@babel/preset-react",
-        {
-          runtime: "classic",
-        },
-      ],
-    ],
-    plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
-  },
-};
-
 export default defineConfig({
-  integrations: [react(reactConfig)],
   output: "server",
   adapter: node({
     mode: "standalone",
   }),
-  security: {
-    checkOrigin: false,
-  },
   server: {
     port: 3000,
   },
