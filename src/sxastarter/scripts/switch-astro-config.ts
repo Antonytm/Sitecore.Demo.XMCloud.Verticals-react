8b1f83f9d9b145fa0707ba869514225af0e4b7bd
Antonytm <antontishchenko@gmail.com>
2024-12-05 22:33:16 +0200

    Fix renaming function

--- src/sxastarter/scripts/switch-astro-config.ts
+++ src/sxastarter/scripts/switch-astro-config.ts
@@ -1,8 +1,6 @@
 import fs from "fs";
 
 if (process.env.VERCEL){
-  // Remove astro.config.mjs
   fs.unlinkSync("./astro.config.mjs");
-  // Rename astro.config.vercel.mjs to astro.config.mjs
-  fs.renameSync("./astro.config.vercel.mjs", "./astro.config.mjs");
+  fs.renameSync("./astro.config.mjs.vercel", "./astro.config.mjs");
 }
