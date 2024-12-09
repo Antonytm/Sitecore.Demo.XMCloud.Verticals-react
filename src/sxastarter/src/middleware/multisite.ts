7463651a0e86c091b91b494e432788057f2271aa
Antonytm <antontishchenko@gmail.com>
2024-12-10 01:09:31 +0200

    Debug information

--- src/sxastarter/src/middleware/multisite.ts
+++ src/sxastarter/src/middleware/multisite.ts
@@ -3,13 +3,16 @@ import config from "@/temp/config";
 import { defineMiddleware } from "astro/middleware";
 
 export const multisite = defineMiddleware((context: any, next) => {
-  if(context.request.url.indexOf(sitePrefixIdentifier) > 0){
+  console.log("-----------------");
+  console.log(context.request);
+  console.log("-----------------");
+  if (context.request.url.indexOf(sitePrefixIdentifier) > 0) {
     return next();
   }
   if (config.sites) {
     const sites = JSON.parse(config.sites);
     for (const site of sites) {
-      if(site.hostName === context.request.url.hostname){
+      if (site.hostName === context.request.url.hostname) {
         const url = `/${sitePrefixIdentifier}${context.request.url.pathname}`;
         return context.rewrite(new Request(url));
       }
