148082a9324a11011ac85017ea065ea4f6434bab
Antonytm <antontishchenko@gmail.com>
2024-12-10 02:35:30 +0200

    More debug information

--- src/sxastarter/middleware.ts
+++ src/sxastarter/middleware.ts
@@ -1,11 +1,12 @@
 import { rewrite } from '@vercel/edge';
  
 export default function middleware(request: Request) {
-  const url = new URL(request.url);
+  console.log(request);
+  console.log(request.url);
+  console.log(new URL(request.url));
 
   console.log("VERCEL EDGE MIDDLEWARE");
-  console.log(url);
-  if (url.pathname.startsWith("/en")) {
+  if (request.url.startsWith("/en")) {
     return rewrite(new URL('/site_Financial', request.url));
   }
   console.log("VERCEL EDGE MIDDLEWARE END");
