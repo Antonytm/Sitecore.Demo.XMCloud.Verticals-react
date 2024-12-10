a2da8be1a32f0d554881cb882e840408b7804708
Antonytm <antontishchenko@gmail.com>
2024-12-10 17:07:30 +0200

    More generic logic + debug

--- src/sxastarter/middleware.ts
+++ src/sxastarter/middleware.ts
@@ -1,22 +1,25 @@
 import { rewrite } from '@vercel/edge';
+import config from "@/temp/config";
 
 export default function middleware(request: Request) {
   console.log(request);
   console.log(request.url);
   console.log(new URL(request.url));
+  console.log(new URL(request.url).host);
+  console.log(new URL(request.url).pathname);
 
   console.log("VERCEL EDGE MIDDLEWARE");
   if (request.url.indexOf(".js") === -1
     && request.url.indexOf(".css") === -1
     && request.url.indexOf("site_") === -1) {
-    if (request.url.startsWith("https://deployment-4nu2c4veudc0r9aare0a-services.vercel.app/")) {
-      return rewrite(request.url.replace("https://deployment-4nu2c4veudc0r9aare0a-services.vercel.app/", "https://deployment-4nu2c4veudc0r9aare0a-services.vercel.app/site_Services/"));
-    }
-    if (request.url.startsWith("https://deployment-4nu2c4veudc0r9aare0a-financial.vercel.app/")) {
-      return rewrite(request.url.replace("https://deployment-4nu2c4veudc0r9aare0a-financial.vercel.app/", "https://deployment-4nu2c4veudc0r9aare0a-financial.vercel.app/site_Financial/"));
-    }
-    if (request.url.startsWith("https://deployment-4nu2c4veudc0r9aare0a-basic.vercel.app/")) {
-      return rewrite(request.url.replace("https://deployment-4nu2c4veudc0r9aare0a-basic.vercel.app/", "https://deployment-4nu2c4veudc0r9aare0a-basic.vercel.app/site_Basic/"));
+    if (config.sites) {
+      const sites = JSON.parse(config.sites);
+      const url = new URL(request.url);
+      for (const site of sites) {
+        if (request.url.indexOf(site.hostName) > 0) {
+          return rewrite(request.url.replace(site.hostName, site.hostName + "/site_" + site.name));
+        }
+      }
     }
   }
   console.log("VERCEL EDGE MIDDLEWARE END");
