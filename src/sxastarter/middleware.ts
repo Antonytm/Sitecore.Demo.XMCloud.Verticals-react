e8257c6109e30f7cdc9f21e515ae6c58470030a5
Antonytm <antontishchenko@gmail.com>
2024-12-11 00:50:04 +0200

    Force lowercase in middleware

--- src/sxastarter/middleware.ts
+++ src/sxastarter/middleware.ts
@@ -35,7 +35,7 @@ export default function middleware(request: Request) {
 
       if (url.host.startsWith(hostname)) {
         path = `/site_${site.name}${path}`;
-        return rewrite(`${url.protocol}//${url.host}${path}`);
+        return rewrite(`${url.protocol}//${url.host}${path}`.toLowerCase());
       }
     }
   }
