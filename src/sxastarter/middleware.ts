f5977463c545fc635a5e627d7c986b45fae42df7
Antonytm <antontishchenko@gmail.com>
2024-12-10 02:39:54 +0200

    Experiment

--- src/sxastarter/middleware.ts
+++ src/sxastarter/middleware.ts
@@ -6,8 +6,8 @@ export default function middleware(request: Request) {
   console.log(new URL(request.url));
 
   console.log("VERCEL EDGE MIDDLEWARE");
-  if (request.url.startsWith("/en")) {
-    return rewrite(new URL('/site_Financial', request.url));
+  if (request.url.startsWith("https://deployment-4nu2c4veudc0r9aare0a-services.vercel.app/")) {
+    return rewrite("https://deployment-4nu2c4veudc0r9aare0a-services.vercel.app/site_Basic/en");
   }
   console.log("VERCEL EDGE MIDDLEWARE END");
 }
