af8f4cb5c9bda8767c1c01eb928205b55ed4cf28
Antonytm <antontishchenko@gmail.com>
2024-12-11 00:48:43 +0200

    Remove logging from middleware.ts

--- src/sxastarter/middleware.ts
+++ src/sxastarter/middleware.ts
@@ -5,7 +5,6 @@ import { languages } from './src/lib/languages.js';
 export default function middleware(request: Request) {
   const url = new URL(request.url.toLowerCase());
 
-  console.log("VERCEL EDGE MIDDLEWARE");
   if (request.url.indexOf(".js") === -1
     && request.url.indexOf(".css") === -1
     && request.url.indexOf(".ico") === -1
@@ -15,18 +14,13 @@ export default function middleware(request: Request) {
       "language": "en",
       "hostName": "deployment",
     }];
-    
+
     for (const site of sites) {
 
       let path = url.pathname;
-
-
       let hasLanguage = false;
-      console.log("VERCEL EDGE MIDDLEWARE LANGUAGE", languages);
       for (const language of languages) {
-        console.log("VERCEL EDGE MIDDLEWARE LANGUAGE", language);
         if (url.pathname.startsWith("/" + language.toLocaleLowerCase())) {
-          console.log("VERCEL EDGE MIDDLEWARE HAS LANGUAGE", language);
           hasLanguage = true;
         }
       }
@@ -39,17 +33,10 @@ export default function middleware(request: Request) {
       // Temporary fix for the issue above
       const hostname = site.hostName.replace("-basic", "-website");
 
-      console.log("VERCEL EDGE MIDDLEWARE HOSTNAME", hostname);
-      console.log("VERCEL EDGE MIDDLEWARE URL HOST", url.host);
-      console.log("VERCEL EDGE MIDDLEWARE URL HOST STARTS WITH HOSTNAME", url.host.startsWith(hostname));
-
       if (url.host.startsWith(hostname)) {
         path = `/site_${site.name}${path}`;
-        console.log("VERCEL EDGE MIDDLEWARE REWRITE", `${url.protocol}//${url.host}${path}`);
         return rewrite(`${url.protocol}//${url.host}${path}`);
       }
-
     }
   }
-  console.log("VERCEL EDGE MIDDLEWARE END");
 }
